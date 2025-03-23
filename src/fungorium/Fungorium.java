70e066c0155224a0a4b3289f98e20bf3278e48d8
Zelch Csaba <zch03po@gmail.com>
2025-03-23 12:30:29 +0100

    megvan

--- src/fungorium/Fungorium.java
+++ src/fungorium/Fungorium.java
@@ -10,7 +10,12 @@ public class Fungorium {
 
         boolean loop = true;
         while (loop == true) {
-            System.out.println("Kell egy szam");
+            System.out.println("7 - Fonál felszívódása tektonról");
+            System.out.println("8 - Tekton kettétörése");
+            System.out.println("9 - Tekton kettétörése, ha a tektonon gombatest van");
+            System.out.println("10 - Gombatesthez nem kapcsolódó fonálrész eltávolítása");
+            System.out.println("11 - Gombatest elpusztulása");
+            System.out.println("12 - Gombafonál elvágása");
             int select = scanner.nextInt();
             switch (select) {
                 case 1:
@@ -46,18 +51,22 @@ public class Fungorium {
                     System.out.println("\n\n");
                     break;
                 case 9:
+                    t.break_tecton_with_mushroom();
+                    System.out.println("\n\n");
+                break;
+                case 10:
                     t.delete_Unnecessary_Threads();
                     System.out.println("\n\n");
                     break;
-                case 10:
-                    t.break_tecton_with_mushroom();
-                    System.out.println("\n\n");
-                    break;
                 case 11:
-                    //t.mushroom_die();
+                    t.mushroom_die();
                     System.out.println("\n\n");
                 break;
                 case 12:
+                    t.cut_fungalthread();
+                    System.out.println("\n\n");
+                break;
+                case 13:
                     loop = false;
                     break;
             }
