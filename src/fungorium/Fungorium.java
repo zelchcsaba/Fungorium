c674dde584627dc255bfff51f4e51a7b5771d144
Zelch Csaba <zch03po@gmail.com>
2025-03-23 13:01:41 +0100

    en vegeztem

--- src/fungorium/Fungorium.java
+++ src/fungorium/Fungorium.java
@@ -10,12 +10,15 @@ public class Fungorium {
 
         boolean loop = true;
         while (loop == true) {
+            
             System.out.println("7 - Fonál felszívódása tektonról");
             System.out.println("8 - Tekton kettétörése");
             System.out.println("9 - Tekton kettétörése, ha a tektonon gombatest van");
             System.out.println("10 - Gombatesthez nem kapcsolódó fonálrész eltávolítása");
             System.out.println("11 - Gombatest elpusztulása");
             System.out.println("12 - Gombafonál elvágása");
+            System.out.print("Kérek egy számot: ");
+
             int select = scanner.nextInt();
             switch (select) {
                 case 1:
--- src/fungorium/Tecton.java
+++ src/fungorium/Tecton.java
@@ -181,7 +181,7 @@ public abstract class Tecton {
      */
     public boolean isNeighbor(Tecton t) {
         //meghívja a tester kiíró függvényét
-        this.t.toCall("cut");
+        this.t.toCall("isNeighbor");
         if (neighbors.contains(t)) {
 
             this.t.returnValue.clear();
