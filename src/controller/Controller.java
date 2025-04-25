754cfef1d9ac0c1103bdce02106bda5bff1b6662
Soma Szolár <szolarsoma@edu.bme.hu>
2025-04-25 18:46:39 +0200

    ellenörzés cut és move-ba

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -268,12 +268,7 @@ public class Controller {
             break;
             }
 
-            case "cut":{ //<Rovarnév> <Tektonnév>
-                if (!(currentPlayer instanceof InsectPlayer)) { // Ebbe nem vagyok biztos, hogy szép e így, vagy, hogy egyáltalán kell e
-                    System.out.println("A 'cut' parancs csak rovarász játékosoknak engedélyezett!");
-                    return;
-                }
-
+            case "cut":{ //<Rovarnév> <Tektonnév>                
                 // Paraméterek kinyerése
                 String insectName = command[1];
                 String tectonName = command[2];
@@ -282,11 +277,25 @@ public class Controller {
                 Tecton tecton = (Tecton)objects.get(tectonName);
                 Insect insect = (Insect)objects.get(insectName);
 
+                // Ő következik?
+                InsectPlayer insectPlayer = null;
+                for(InsectPlayer iPlayer : insectPlayers){
+                    for(InsectAssociation insectA : iPlayer.getInsects()){
+                        if(insectA.getInsect() == insect){
+                            insectPlayer = iPlayer;
+                        }
+                    }
+                }
+
+                if(insectPlayer != currentPlayer){
+                    System.out.println("Ez nem a te rovarad!");
+                    return;
+                }
+
                 // Segéd objektumok
-                InsectPlayer insectPlayer = (InsectPlayer) currentPlayer;
                 InsectAssociation insectAssociation = insectPlayer.getInsectAssociation(insect);
                 
-                // Az ő rovarával akar vágni?
+                // Az ő rovarával akar vágni? (lehet fölösleges)
                 if (insectAssociation == null) {
                     System.out.println("Ez a rovar nem a játékosodhoz tartozik.");
                     return;
@@ -418,13 +427,7 @@ public class Controller {
                 break;
             }
 
-
             case "move":{ //<Rovarnév> <Tektonnév>
-                if (!(currentPlayer instanceof InsectPlayer)) { // Ebbe nem vagyok biztos, hogy szép e így, vagy, hogy egyáltalán kell e
-                    System.out.println("A 'move' parancs csak rovarász játékosoknak engedélyezett!");
-                    return;
-                }
-
                 // Paraméterek kinyerése
                 String insectName = command[1];
                 String tectonName = command[2];
@@ -433,16 +436,31 @@ public class Controller {
                 Tecton tecton = (Tecton)objects.get(tectonName);
                 Insect insect = (Insect)objects.get(insectName);
 
+                // Ő következik?
+                InsectPlayer insectPlayer = null;
+                for(InsectPlayer iPlayer : insectPlayers){
+                    for(InsectAssociation insectA : iPlayer.getInsects()){
+                        if(insectA.getInsect() == insect){
+                            insectPlayer = iPlayer;
+                        }
+                    }
+                }
+
+                if(insectPlayer != currentPlayer){
+                    System.out.println("Ez nem a te rovarad!");
+                    return;
+                }
+                
                 // Segéd objektumok
                 List<Spore> spores = tecton.getSpores();
-                InsectPlayer insectPlayer = (InsectPlayer) currentPlayer;
                 InsectAssociation insectAssociation = insectPlayer.getInsectAssociation(insect);
                 
-                // Az ő rovarával akar lépni?
+                // Az ő rovarával akar lépni? (lehet fölösleges)
                 if (insectAssociation == null) {
                     System.out.println("Ez a rovar nem a játékosodhoz tartozik.");
                     return;
                 }
+
                 // Tud lépni?
                 if (insectAssociation.getMoved()) {
                     System.out.println("Ez a rovar már lépett ebben a körben.");
