49e816825e52e94d870677f7bf3cc37e5097c394
Soma Szolár <szolarsoma@edu.bme.hu>
2025-05-08 23:08:15 +0200

    GamePanel struktúrálása

--- src/view/GamePanel.java
+++ src/view/GamePanel.java
@@ -1,26 +1,18 @@
 package view;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.swing.JButton;
-
-import javax.swing.JPanel;
-
 import controller.Controller;
-import model.Tecton;
-
-
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import model.Tecton;
 
 
 public class GamePanel extends JPanel{
     private MainWindow parent;
-
+    DrawingPanel drawingPanel;
     Controller controller;
 
     private java.util.List<GTecton> polygons = new ArrayList<>();
@@ -28,10 +20,52 @@ public class GamePanel extends JPanel{
     public GamePanel(MainWindow parent, Controller controller) {
         this.parent = parent;
         this.controller = controller;
+
+        setLayout(new BorderLayout()); 
+
+        // játékos, körszám
+        JPanel topPanel = new JPanel(new BorderLayout());
+        topPanel.setBackground(Color.CYAN);
+        add(topPanel, BorderLayout.NORTH);
+
+        // játékos
+        JPanel playerPanel = new JPanel(new FlowLayout());
+        playerPanel.setBackground(Color.CYAN);
+        topPanel.add(playerPanel, BorderLayout.WEST);
+
+        JLabel currentPlayer = new JLabel("Current Player:");
+        currentPlayer.setForeground(Color.BLACK);
+        playerPanel.add(currentPlayer);
         
+        JLabel player = new JLabel("Huszi");
+        player.setForeground(Color.BLACK); 
+        playerPanel.add(player);
+
+        // kör
+        JPanel roundPanel = new JPanel(new FlowLayout());
+        roundPanel.setBackground(Color.CYAN);
+        topPanel.add(roundPanel, BorderLayout.EAST);
+
+        JLabel roundNumber = new JLabel("Round:");
+        roundNumber.setForeground(Color.BLACK);
+        roundPanel.add(roundNumber);
+
+        JLabel number = new JLabel("1");
+        number.setForeground(Color.BLACK);
+        roundPanel.add(number);
+
+        drawingPanel = new DrawingPanel();
+        drawingPanel.setBackground(Color.WHITE); // vagy amit szeretnél
+        add(drawingPanel, BorderLayout.CENTER);
+
+        // akció gombok
+        JPanel actionsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));    
+        actionsPanel.setBackground(Color.BLACK);    
+        add(actionsPanel, BorderLayout.SOUTH);
+
         JButton startButton = new JButton("Anyád");
         startButton.addActionListener(e -> parent.showStartScreen());
-        this.add(startButton);
+        actionsPanel.add(startButton);
 
         addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
@@ -39,7 +73,7 @@ public class GamePanel extends JPanel{
                 for (GTecton poly : polygons) {
                     if (poly.contains(click)) {
                         poly.toggleSelected();
-                        repaint();
+                        drawingPanel.repaint();
                         break;
                     }
                 }
@@ -59,6 +93,7 @@ public class GamePanel extends JPanel{
         gtect.setLineCount(lineCount);
         gtect.setTecton(t);
         polygons.add(gtect);
+        drawingPanel.repaint();
     }
 
     protected void paintComponent(Graphics g) {
@@ -67,4 +102,16 @@ public class GamePanel extends JPanel{
             poly.draw(g);
         }
     }
+
+    public class DrawingPanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            for (GTecton poly : polygons) {
+                poly.draw(g);
+            }
+        }
+    }
 }
+
+
