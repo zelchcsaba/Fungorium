373e96fe7a3f0797b6fbd24b1095436d3f6a8e69
Zelch Csaba <zch03po@gmail.com>
2025-05-09 20:07:18 +0200

    jah

--- src/view/WinPanel.java
+++ src/view/WinPanel.java
@@ -21,7 +21,7 @@ import javax.swing.SwingConstants;
 import javax.swing.border.Border;
 
 import controller.Controller;
-import view.GamePanel.DrawingPanel;
+
 
 public class WinPanel extends JPanel{
     private MainWindow parent;
